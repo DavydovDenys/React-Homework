22907c4aff9dc040f586c621eb6f4f59ec546199
DavydovDenys <denvanrain@gmail.com>
2020-08-02 22:02:06 +0300

    update the components

---
+++ src/UserInput/UserInput.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const UserInput = () => {
+  return (
+    <div className='UserInput'>
+      <input type="text"/>
+    </div>
+  )
+}
---
+++ src/UserOutput/UserOutput.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const UserOutput = () => {
+  return (
+    <div className='UserOutput'>
+      <p></p>
+      <p></p>
+    </div>
+  )
+}
